9618fa6ab267a2614aa43b05b92ba29023a90697
tperale <perale.thomas@gmail.com>
2020-08-30 17:31:33 +0200

    [enh] More error checking

--- victoria/__main__.py
+++ victoria/__main__.py
@@ -1,57 +1,105 @@
-from time import sleep
 from daemonize import Daemonize
-from aioconsole import ainput
-from evdev import InputDevice, categorize, ecodes
 from jinja2 import Environment, PackageLoader
+from jinja2.exceptions import TemplateNotFound
 import uuid
-import signal
-import asyncio
 import logging
 import argparse
 import redis
 import time
 import json
 
-pid = "/tmp/victoria.pid"
+APPNAME = "victoria"
+
 r = redis.Redis(host='localhost', port=6379, db=0)
 p = r.pubsub()
 
 # Jinja2 Init
 env = Environment(
-    loader=PackageLoader(__name__, 'templates')
+    loader=PackageLoader('victoria','templates')
 )
 
+logger = logging
+
+def retry():
+    MAX_RETRY = 30
+    retry_number = 1
+    while retry_number:
+        time.sleep(min(retry_number, MAX_RETRY))
+        try:
+            p.subscribe('printer')
+            return
+        except redis.exceptions.ConnectionError:
+            logger.warning("Redis server retry attempt nº%i." % retry_number)
+            retry_number += 1
+            continue
 
 def main():
-    p.subscribe('printer')
+    try:
+        p.subscribe('printer')
+    except redis.exceptions.ConnectionError:
+        logger.warning("Failed to connect to redis server. Retrying.")
+        retry()
 
     while True:
-        message = p.get_message()
-        if message and message['type'] != 'subscribe':
-            info = json.loads(message['data'])
-            template = env.get_template('productbarcode40x100.zpl')
+        try:
+            message = p.get_message()
+        except redis.exceptions.ConnectionError:
+            logger.warning("Redis server disconnected. Retrying.")
+            retry()
+        if message and (message['type'] == 'message' or message['type'] == 'pmessage'):
+            try:
+                info = json.loads(message['data'])
+            except json.decoder.JSONDecodeError as e:
+                logger.error("Failed to decode json data from redis: %s" % e)
+                continue
 
-            filename = '/tmp/%s.zpl' % (str(uuid.uuid4()))
-            file = open(filename, 'w')
-            # Get part name from database.
-            file.write(str(template.render(name=info.get('name', ''), number=info['barcode'])))
-            file.close()
+            try:
+                template = env.get_template('productbarcode40x100.zpl')
+            except TemplateNotFound:
+                logger.error("Template not found")
+                continue
 
-            logging.info("Launching the print of the barcode: %s" % (info['barcode']))
+            filename = '/tmp/%s-%s.zpl' % (APPNAME, str(uuid.uuid4()))
+            try:
+                f = open(filename, 'w')
+            except OSError:
+                logger.error("Error opening the filename '%s'" % filename)
+                continue
+            with f:
+                # TODO Get part name from database and do verification.
+                f.write(str(template.render(name=info.get('name', ''), number=info['barcode'])))
+
+            logger.info("Launching the print of the barcode: %s" % (info['barcode']))
 
             # sp.check_output(['lpr', '-P', 'zebra', '-o', 'raw', filename])
+        elif message:
+            logger.debug(str(message))
         time.sleep(0.01)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--no-daemon', dest='nodaemon', action='store_true', help='Does not start the program as a daemon')
+    parser.add_argument('--logfile', dest='logfile', type=str, help='Log destination', default=("/var/log/%s.log" % APPNAME))
+    parser.add_argument('--pid', dest='pid', type=str, help='Pid destination', default=("/tmp/%s.pid" % APPNAME))
+
     args = parser.parse_args()
 
     if args.nodaemon:
-        logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+        logger.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
         main()
     else:
-        logging.basicConfig(filename='/var/log/victoria.log', format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
-        daemon = Daemonize(app="erie", pid=pid, action=main)
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        logger.propagate = False
+
+        fh = logging.FileHandler(args.logfile, "w")
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(fmt='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        fh.setFormatter(formatter)
+
+        logger.addHandler(fh)
+        keep_fds = [fh.stream.fileno()]
+
+        daemon = Daemonize(app=APPNAME, logger=logger, pid=args.pid, action=main, keep_fds=keep_fds)
         daemon.start()
